1f69947823f1c111d48d1f330a5484ce3e43fdd1
MohammadrezaFarkhondeh <mohammr.farkhonde12@sharif.edu>
2025-08-08 00:31:57 +0330

    feat: update main activity to use app nav host

--- app/src/main/java/com/waiotech/android/MainActivity.kt
+++ app/src/main/java/com/waiotech/android/MainActivity.kt
@@ -3,15 +3,11 @@ package com.waiotech.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.waiotech.android.navigation.AppNavHost
 import com.waiotech.android.ui.theme.WaiotechTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,28 +19,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    HelloCompose()
+                    AppNavHost()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun HelloCompose(modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
-    ) {
-        Text(text = "Hello, Compose!")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WaiotechTheme {
-        HelloCompose()
     }
 }
 
