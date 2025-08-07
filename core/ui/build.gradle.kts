7247e32ec94de9b8485219ba199bc1ee9d5048b6
MohammadrezaFarkhondeh <mohammr.farkhonde12@sharif.edu>
2025-08-08 00:31:17 +0330

    feat: updated dependencies for navhost

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,10 +1,31 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+    id("com.android.library")
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.waiotech.android.core.ui"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
 }
 
 dependencies {
-    // UI components and themes will be added here
-    // Will depend on :core:common
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
 }
 
 kotlin {
